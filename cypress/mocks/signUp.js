59bab0956c110372df9754028c4b45fcb414ef62
Maria Borowska <maria.borowska@ergodit.com>
2024-05-07 13:39:41 +0200

    refactor intercepts

---
+++ cypress/mocks/signUp.js
@@ -0,0 +1,14 @@
+export const signUpMocks = {
+
+    mockSuccess: () => {
+        cy.intercept('POST', '**/users/signup', (req) => {
+            req.reply({
+                statusCode: 201,
+                body: {
+                    token: 'fakeToken'
+                }
+            })
+        })
+    }
+
+}
